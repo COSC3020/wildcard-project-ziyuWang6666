06d938c187a4be9584c1b9659fcd13af88f36d32
ziyuWang6666 <144853480+ziyuWang6666@users.noreply.github.com>
2023-12-08 16:46:47 -0700

    Update code.js
    adjacent matrix data structure

--- code.js
+++ code.js
@@ -1,54 +1,54 @@
-function mkSet(parent,rank,n) { 
-  //function to initialize parent[] and rank[]
-	for(let i=0;i<n;i++) { 
-		parent[i]=i;
-		rank[i]=0;
-	} 
-} 
-
 function findParent(parent,component) { 
 	//function is to find the root of the sets
-	if(parent[component]==component) 
-		return component; 
-
+	if(parent[component]==component) return component; 
 	return parent[component] = findParent(parent,parent[component]); 
 } 
 // merge two disjoint sets based on their ranks.
 function unionSet(u, v, parent, rank) {
-	if(rank[u]<rank[v]) {
-		parent[u]=v;
-	} else if(rank[v]<rank[u]) {
-		parent[v]=u;
-	} else{
-		parent[v]=u; 
-		rank[u]++;//since the rank increases if the ranks of two sets are same 
-	} 
-} 
+	let rootU = findParent(parent, u);
+    	let rootV = findParent(parent, v);
 
-function kruskalAlgo(edge) {
-  let n = edge.length;
+	if(rank[rootU]<rank[rootV]) {
+		parent[rootU]=rootV;
+	} else if(rank[rootV]<rank[rootU]) {
+		parent[rootV]=rootU;
+	} else{
+		parent[rootV]=rootU; 
+		rank[rootU]++;//since the rank increases if the ranks of two sets are same 
+	} 
+}
+
+function kruskalAlgo(ajMatrix) {
+  let n = ajMatrix.length;
   if(n < 2) return 0;
-	//Sort the edge array in ascending order, so we can access minimum distances/cost 
-	edge.sort((a, b)=>{
-		return a[2] - b[2];
-	})
 	let parent = new Array(n), rank = new Array(n);
 
-	mkSet(parent,rank,n);
+    for (let i = 0; i < n; i++) {
+        parent[i] = i;
+        rank[i] = 0;
+    }
+	let edges = [];
+	// Set to a edges list
+    for (let i = 0; i < n; i++) {
+        for (let j = i + 1; j < n; j++) {
+            let wt = ajMatrix[i][j];//weight
+            if (wt !== 0) {
+                edges.push({ u: i, v: j, weight: wt });
+            }
+        }
+    }
+	edges.sort((a, b) => a.weight - b.weight);
 
 	let minCost=0;
-
-	for(let i=0;i<n;i++) {
-		let v1=findParent(parent,edge[i][0]);
-		let v2=findParent(parent,edge[i][1]);
-		let wt=edge[i][2];
-    //if the parents are different that means they are in different sets so union them 
-		if(v1!=v2) {
+	for(let edge of edges) {
+		let {u, v, weight} = edge;
+		let v1=findParent(parent,u);
+		let v2=findParent(parent,v);
+		if(v1 !== v2) {
+			//if the parents are different that means they are in different sets so union them 
 			unionSet(v1,v2,parent,rank);
-			minCost+=wt;
+			minCost+=weight;
 		}
 	}
-  return minCost;
+  	return minCost;
 }
-// required sources
-// https://www.geeksforgeeks.org/kruskals-minimum-spanning-tree-algorithm-greedy-algo-2/
